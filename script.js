5c41f2cd1489eaab2a1edbb483217bd7eccc4967
ShankarKalvankar <165810143+ShankarKalvankar@users.noreply.github.com>
2024-04-02 21:43:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,135 @@
+function addNewCLkField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("clkField");
+    
+
+    let clkOb = document.getElementById("clk");
+    let clkAddButtonOb = document.getElementById("clkAddButton");
+
+    clkOb.insertBefore(newNode, clkAddButtonOb);
+}  
+
+function addNewWField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("WField");
+    let WOb = document.getElementById("W");
+    let WAddButtonOb = document.getElementById("WAddButton");
+
+    WOb.insertBefore(newNode, WAddButtonOb);
+}  
+
+function addNewCNField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("CNField");
+    let CNOb = document.getElementById("CN");
+    let CNAddButtonOb = document.getElementById("CNAddButton");
+
+    CNOb.insertBefore(newNode, CNAddButtonOb);
+}  
+
+function addNewYEField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("YEField");
+    
+
+    let YEOb = document.getElementById("YE");
+    let YEAddButtonOb = document.getElementById("YEAddButton");
+
+    YEOb.insertBefore(newNode, YEAddButtonOb);
+}  
+
+function addNewPField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("PField");
+    
+
+    let POb = document.getElementById("P");
+    let PAddButtonOb = document.getElementById("PAddButton");
+
+    POb.insertBefore(newNode, PAddButtonOb);
+}  
+
+function addNewPRField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("PRField");
+    
+
+    let clkOb = document.getElementById("PR");
+    let clkAddButtonOb = document.getElementById("PRAddButton");
+
+    PROb.insertBefore(newNode, PRAddButtonOb);
+}  
+
+function addNewCLField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("CLField");
+    
+
+    let clkOb = document.getElementById("CL");
+    let clkAddButtonOb = document.getElementById("CLAddButton");
+
+    clkOb.insertBefore(newNode, CLAddButtonOb);
+}  
+
+function addNewCLkField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("clkField");
+    
+
+    let clkOb = document.getElementById("clk");
+    let clkAddButtonOb = document.getElementById("clkAddButton");
+
+    clkOb.insertBefore(newNode, clkAddButtonOb);
+}  
+
+function addNewCLkField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("clkField");
+    
+
+    let clkOb = document.getElementById("clk");
+    let clkAddButtonOb = document.getElementById("clkAddButton");
+
+    clkOb.insertBefore(newNode, clkAddButtonOb);
+}  
+
+function addNewCLkField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("clkField");
+    
+
+    let clkOb = document.getElementById("clk");
+    let clkAddButtonOb = document.getElementById("clkAddButton");
+
+    clkOb.insertBefore(newNode, clkAddButtonOb);
+}  
+
+function addNewCLkField() {
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("clkField");
+    
+
+    let clkOb = document.getElementById("clk");
+    let clkAddButtonOb = document.getElementById("clkAddButton");
+
+    clkOb.insertBefore(newNode, clkAddButtonOb);
+}  
+
+
+
+//generate resume
+function generateResume() {
+    console.log("Generate Resume")
+}
+
